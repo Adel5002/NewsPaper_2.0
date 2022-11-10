ce8614106a49e0de13bca94e6c76920335e5b289
Adel5002 <79848315+Adel5002@users.noreply.github.com>
2022-11-10 13:53:58 +0300

    Update settings.py

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_*2ks^40^e2zah(vhd#ei1$z0eokg7*!x12&m*jeqa*l$6dm7g'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
